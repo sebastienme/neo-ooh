b4e8d55dcd80cbf1576dadd5705889fb9b32bf98
sebastien <sebastien.meslage@gmail.com>
2023-01-31 17:05:03 -0500

    add effect on employe page

---
+++ employeScript.js
@@ -0,0 +1,60 @@
+let employe = [
+    {
+        "name" : "Ronald Tapiero",
+        "id" : "1",
+        "imageSrc" : "Ronald.jpg",
+        "altAtt" : "Ronald Tapiero",
+        "title" : "President, General Manager",
+        "description" : "Now an executive with Neo-ooh. Josee joined the team in 2019 just before the pandemic She enjoys her motorcycle and long talks with Ronald and the staff about ongoing projects and where the industry is going. She has a vast experience in this field.",
+        "linkedin" : "https://www.linkedin.com/company/neo-ooh/"
+    },
+    {
+        "name" : "Ziad Balti",
+        "id" : "2",
+        "imageSrc" : "Ziad.jpg",
+        "altAtt" : "Ziad Balti",
+        "title" : "VP Finance, Numbers guy",
+        "description" : "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
+        "linkedin" : "https://www.linkedin.com/"
+    },
+    {
+        "name" : "Luc Quetel",
+        "id" : "3",
+        "imageSrc" : "Luc.jpg",
+        "altAtt" : "Luc Quetel",
+        "title" : "Executive VP Partner",
+        "description" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        "linkedin" : "https://www.linkedin.com/company/neo-ooh/"
+    }
+
+];
+
+const employeDivImg = document.querySelectorAll('.employe-div > img');
+
+employeDivImg.forEach((element) => {
+    element.addEventListener('click', function () {
+
+        const employeId = this.getAttribute('data-id');
+        generateHeaderEmploye(employeId);
+
+    })
+})
+
+function generateHeaderEmploye(idOfEmploye) {
+    const name = document.querySelector('.header-employe__details__items h1');
+    const bottomName = document.querySelector('.employe__bottom__personal__name');
+    const title = document.querySelector('.header-employe__details__items__role');
+    const description = document.querySelector('.employe__bottom > p');
+    const image = document.querySelector('.header-employe__image > img');
+    const linkedinLink = document.querySelector('.employe__bottom__personal > a');
+
+    let employeToDisplay = employe.find(emp => emp.id == idOfEmploye);
+    
+    name.innerHTML = `${employeToDisplay.name}`;
+    bottomName.innerHTML = `${employeToDisplay.name}`;
+    title.innerHTML = `${employeToDisplay.title}`;
+    description.innerHTML = `${employeToDisplay.description}`;
+    image.src = `images/${employeToDisplay.imageSrc}`;
+    linkedinLink.setAttribute('href', employeToDisplay.linkedin);
+
+}
